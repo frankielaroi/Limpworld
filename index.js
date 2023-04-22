1be26ddc75e9458969a4184f28a9b0a568c43af3
jakesgreen <jakesevergreen999@gmail.com>
2023-04-22 11:17:04 +0000

    AVENGERS  loki

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-window.addEventListener("scroll", function() {
-  var offset = window.pageYOffset;
-  var parallax = document.querySelector(".logo-bg");
-  parallax.style.backgroundPositionY = offset * 0.7 + "px";
-});
+let carousel = document.querySelector('.carousel');
+let container = carousel.querySelector('.container');
+let item = carousel.querySelectorAll('.items');
+let items = [...item];
+
+window.onload = function slider(){
+    setInterval(slide, 6000)
+    function slide() {
+    
+        for (let i = 0; i < items.length; i++){
+            items.forEach((e) => {
+                e.classList.remove(`item${i + 1}`);
+            });
+            items[i].classList.add(`item${i + 1}`);
+        }            
+        let e = items.shift();
+        items.push(e);
+          
+    }
+    
+}
---
+++ junk
@@ -0,0 +1,5 @@
+window.addEventListener("scroll", function() {
+  var offset = window.pageYOffset;
+  var parallax = document.querySelector(".logo-bg");
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+});
